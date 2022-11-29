7ee9670a0b02c073ecba37ffa1027328cc8bd892
Abhinay <kunduruabhinayreddy43@gmail.com>
2022-11-30 00:36:26 +0530

    Second Commit after using POST using postman to send POST requests and storing that values in sqlite DB. and adding styling to webpage using HTML and CSS

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-from flask import Flask, request, jsonify
+import sqlite3
 
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
-L = list()
 
+L = list()
 @app.route("/")
-def hello_world():
-    return "<h1>Farmers Registry!</h1>"
+def index():
+    sql_query = 'SELECT * FROM FARMER_DETAILS;'
+    curser = sqlite3.connect('identifier.sqlite')
+    curser.execute(sql_query)
+
+
+    return render_template('index.html', name='PyCharm')
 
 @app.route('/farmer', methods=['POST'])
 def create_farmer():
     content = request.json
     print(content)
     L.append(content)
+
+    connection = sqlite3.connect('identifier.sqlite')
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
+
+    cur = connection.cursor()
+    for i in range(len(L)):
+        cur.execute("INSERT INTO FARMER_DETAILS (FARMER_ID,FARMER_NAME,MOBILE_NUMBER,VILLAGE_NAME,ADDRESS) VALUES (?, ?,?,?,?)",(L[i]['FARMER_ID'],L[i]['FARMER_NAME'],L[i]['MOBILE_NUMBER'],L[i]['VILLAGE'],L[i]['ADDRESS']))
+    connection.commit()
+    connection.close()
+
     return jsonify("Succss")
 
-# @app.route('/get_farmer_data')
-# def send_data():
-#     return jsonify()
+@app.route('/get_farmer_data', methods=['GET'])
+def send_data():
+    return jsonify(L)
+
+
