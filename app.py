e376fa720603b916a978970ff2e4dbfce59b7d6e
Abhinay <kunduruabhinayreddy43@gmail.com>
2022-12-01 22:23:15 +0530

    Made more features for user authentication and built the corresponding forms.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Flask, request, jsonify, render_template,url_for,redirect
+from flask import Flask, request, jsonify, render_template,redirect
 app = Flask(__name__)
 
 L = list()
@@ -12,33 +12,55 @@ def index():
 @app.route("/login",methods=['GET','POST'])
 def login():
     connection = sqlite3.connect('identifier.sqlite')
-
-    cur = connection.cursor()
-    cur.execute("SELECT EMAIL FROM SINEUP")
-    all_emails = cur.fetchall()
-
-
     error = None
+
     if request.method == 'POST':
-        if tuple(request.form['Email']) not in all_emails or request.form['password'] != cur.execute("SELECT PASSWORD FROM SINEUP WHERE EMAIL=request.form['Email']").fetchall()[0][0]:
-            error = 'Invalid credintials Try again'
-        else:
-            return redirect('/')
+        cur = connection.cursor()
+        cur.execute("SELECT * FROM SINEUP")
+        users = cur.fetchall()
+
+        for user in users:
+            if request.form['Email'] == user[0] and request.form['password'] == user[1]:
+                return redirect('/')
+
+        error = 'Invalid credintials Try again'
+
     return render_template('login.html', error=error)
 
-@app.route('/farmer', methods=['POST'])
+@app.route('/sineup',methods=['POST','GET'])
+def sineup():
+    connection = sqlite3.connect('identifier.sqlite')
+
+    if request.method =='POST':
+        cur = connection.cursor()
+        cur.execute("INSERT INTO SINEUP VALUES (?,?)",(request.form['Email'],request.form['password']))
+        connection.commit()
+        connection.close()
+        return redirect('/login')
+    return render_template('sineup.html')
+
+
+@app.route('/farmer', methods=['POST','PUT'])
 def create_farmer():
     content = request.json
-    print(content)
-    L.append(content)
+    # print(content)
+    # L.append(content)
+    # with open('schema.sql') as f:
+    # connection.executescript(f.read())
 
     connection = sqlite3.connect('identifier.sqlite')
-    with open('schema.sql') as f:
-        connection.executescript(f.read())
 
     cur = connection.cursor()
-    for i in range(len(L)):
-        cur.execute("INSERT INTO FARMER_DETAILS (FARMER_ID,FARMER_NAME,MOBILE_NUMBER,VILLAGE_NAME,ADDRESS) VALUES (?, ?,?,?,?)",(L[i]['FARMER_ID'],L[i]['FARMER_NAME'],L[i]['MOBILE_NUMBER'],L[i]['VILLAGE'],L[i]['ADDRESS']))
+
+    if request.method == 'PUT':
+        # print(content)
+        cur.execute("UPDATE FARMER_DETAILS SET FARMER_NAME= ?, MOBILE_NUMBER =?,VILLAGE_NAME =?, ADDRESS=?  WHERE FARMER_ID=?",
+                    (content['FARMER_NAME'],content['MOBILE_NUMBER'],content['VILLAGE'],content['ADDRESS'],content['FARMER_ID']))
+
+    elif request.method == 'POST':
+        for i in range(len(L)):
+            cur.execute("INSERT INTO FARMER_DETAILS (FARMER_ID,FARMER_NAME,MOBILE_NUMBER,VILLAGE_NAME,ADDRESS) "
+                    "VALUES (?, ?,?,?,?)",(L[i]['FARMER_ID'],L[i]['FARMER_NAME'],L[i]['MOBILE_NUMBER'],L[i]['VILLAGE'],L[i]['ADDRESS']))
     connection.commit()
     connection.close()
 
@@ -52,7 +74,3 @@ def farmer_details():
     cur.execute("SELECT * FROM FARMER_DETAILS")
     rows = cur.fetchall()
     return render_template('farmer_details.html', items=rows)
-
-
-
-
